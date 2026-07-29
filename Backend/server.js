23cc11172b618c92949821088ffc5bc2cea134e2
Kavan Desai <kavandesai006@gmail.com>
2026-07-29 18:34:51 +0530

    Fixed syntax error. V1.2

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 // Global Middleware
 // Configured to allow requests from your Vite frontend
 app.use(cors({ 
-  origin: ['http://localhost:5173', process.env.FRONTEND_URL]
+  origin: ['http://localhost:5173', process.env.FRONTEND_URL],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
